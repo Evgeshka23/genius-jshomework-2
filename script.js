1b6077f139c4b01bf805b23e01765604c37e6fb5
Evgeshka23 <143422172+Evgeshka23@users.noreply.github.com>
2025-03-01 20:01:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Task 1
+
+// const fruts = [
+//     { id: 0, name: "Apple" },
+//     { id: 1, name: "Tomat" },
+//     { id: 2, name: "Cherry" },
+//     { id: 3, name: "Orange" }
+// ];
+// let names = fruts.map(frut => frut.name);
+
+// Task 2
+
+// let number = 10;
+
+// for(let i = 0; i < number; i++){
+//     (i % 2 == 0) ? console.log(i) : null;
+// }
+
+// Task 3
+// let i = 0; 
+// while(i < 5){
+//     console.log(`i = ${i}`);
+//     i++;
+// }
+
+// Task 4
+// let num;
+// do {
+//     num = prompt("Enter number more than 100", 0);
+// } while (num <= 100);
+
+// Task 5
+// const girls = [
+//     { age: 23, name: "Оля" },
+//     { age: 29, name: "Аня" },
+//     { age: 10, name: "Юля" },
+//     { age: 20, name: "Катя" },
+// ];
+
+// let result = girls.reduce((sum, item) => sum + item.age , 0) / girls.length;
